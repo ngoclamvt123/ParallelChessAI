c3015e62b74f0dd8df3a986f29e891c5f3fdcfb2
Rohit Ahuja <rahuja95@gmail.com>
2015-12-01 18:44:51 -0500

    Got rid of unnecessary sunfish code

--- sunfish.py
+++ sunfish.py
@@ -17,20 +17,8 @@ pyximport.install(setup_args={"include_dirs":np.get_include()},
 import chess
 # from minimax import *
 
-
-# The table size is the maximum number of elements in the transposition table.
-TABLE_SIZE = 1e6
-
 # This is the max depth we want our minimax to search
 DEPTH = 2
-
-# This constant controls how much time we spend on looking for optimal moves.
-NODES_SEARCHED = 1e4
-
-# Mate value must be greater than 8*queen + 2*(rook+knight+bishop)
-# King value is set to twice this value such that if the opponent is
-# 8 queens up, but we got the king, we still exceed MATE_VALUE.
-MATE_VALUE = 30000
 
 # Our board is represented as a 120 character string. The padding allows for
 # fast detection of moves that don't stay within the board.
@@ -58,6 +46,7 @@ char_map = { '\n': -3, ' ': -2, '.': -1,
 int_map = { -3: '\n', -2: ' ', -1: '.',
             0: 'p', 1: 'n', 2: 'b', 3: 'r', 4: 'q', 5: 'k',
             6: 'P', 7: 'N', 8: 'B', 9: 'R', 10: 'Q', 11: 'K' }
+
 ###############################################################################
 # Move and evaluation tables
 ###############################################################################
@@ -71,81 +60,6 @@ directions = {
     'Q': (N, E, S, W, N+E, S+E, S+W, N+W),
     'K': (N, E, S, W, N+E, S+E, S+W, N+W)
 }
-pst = {
-    'P': (0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 198, 198, 198, 198, 198, 198, 198, 198, 0,
-        0, 178, 198, 198, 198, 198, 198, 198, 178, 0,
-        0, 178, 198, 198, 198, 198, 198, 198, 178, 0,
-        0, 178, 198, 208, 218, 218, 208, 198, 178, 0,
-        0, 178, 198, 218, 238, 238, 218, 198, 178, 0,
-        0, 178, 198, 208, 218, 218, 208, 198, 178, 0,
-        0, 178, 198, 198, 198, 198, 198, 198, 178, 0,
-        0, 198, 198, 198, 198, 198, 198, 198, 198, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-    'B': (0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 797, 824, 817, 808, 808, 817, 824, 797, 0,
-        0, 814, 841, 834, 825, 825, 834, 841, 814, 0,
-        0, 818, 845, 838, 829, 829, 838, 845, 818, 0,
-        0, 824, 851, 844, 835, 835, 844, 851, 824, 0,
-        0, 827, 854, 847, 838, 838, 847, 854, 827, 0,
-        0, 826, 853, 846, 837, 837, 846, 853, 826, 0,
-        0, 817, 844, 837, 828, 828, 837, 844, 817, 0,
-        0, 792, 819, 812, 803, 803, 812, 819, 792, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-    'N': (0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 627, 762, 786, 798, 798, 786, 762, 627, 0,
-        0, 763, 798, 822, 834, 834, 822, 798, 763, 0,
-        0, 817, 852, 876, 888, 888, 876, 852, 817, 0,
-        0, 797, 832, 856, 868, 868, 856, 832, 797, 0,
-        0, 799, 834, 858, 870, 870, 858, 834, 799, 0,
-        0, 758, 793, 817, 829, 829, 817, 793, 758, 0,
-        0, 739, 774, 798, 810, 810, 798, 774, 739, 0,
-        0, 683, 718, 742, 754, 754, 742, 718, 683, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-    'R': (0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 1258, 1263, 1268, 1272, 1272, 1268, 1263, 1258, 0,
-        0, 1258, 1263, 1268, 1272, 1272, 1268, 1263, 1258, 0,
-        0, 1258, 1263, 1268, 1272, 1272, 1268, 1263, 1258, 0,
-        0, 1258, 1263, 1268, 1272, 1272, 1268, 1263, 1258, 0,
-        0, 1258, 1263, 1268, 1272, 1272, 1268, 1263, 1258, 0,
-        0, 1258, 1263, 1268, 1272, 1272, 1268, 1263, 1258, 0,
-        0, 1258, 1263, 1268, 1272, 1272, 1268, 1263, 1258, 0,
-        0, 1258, 1263, 1268, 1272, 1272, 1268, 1263, 1258, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-    'Q': (0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 2529, 2529, 2529, 2529, 2529, 2529, 2529, 2529, 0,
-        0, 2529, 2529, 2529, 2529, 2529, 2529, 2529, 2529, 0,
-        0, 2529, 2529, 2529, 2529, 2529, 2529, 2529, 2529, 0,
-        0, 2529, 2529, 2529, 2529, 2529, 2529, 2529, 2529, 0,
-        0, 2529, 2529, 2529, 2529, 2529, 2529, 2529, 2529, 0,
-        0, 2529, 2529, 2529, 2529, 2529, 2529, 2529, 2529, 0,
-        0, 2529, 2529, 2529, 2529, 2529, 2529, 2529, 2529, 0,
-        0, 2529, 2529, 2529, 2529, 2529, 2529, 2529, 2529, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-    'K': (0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 60098, 60132, 60073, 60025, 60025, 60073, 60132, 60098, 0,
-        0, 60119, 60153, 60094, 60046, 60046, 60094, 60153, 60119, 0,
-        0, 60146, 60180, 60121, 60073, 60073, 60121, 60180, 60146, 0,
-        0, 60173, 60207, 60148, 60100, 60100, 60148, 60207, 60173, 0,
-        0, 60196, 60230, 60171, 60123, 60123, 60171, 60230, 60196, 0,
-        0, 60224, 60258, 60199, 60151, 60151, 60199, 60258, 60224, 0,
-        0, 60287, 60321, 60262, 60214, 60214, 60262, 60321, 60287, 0,
-        0, 60298, 60332, 60273, 60225, 60225, 60273, 60332, 60298, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
-}
-
 
 ###############################################################################
 # Chess logic
@@ -190,29 +104,6 @@ class Position(namedtuple('Position', 'board score wc bc ep kp')):
         pos = chess.make_move(args, np.array(move).astype(np.int32))
         return Position(self.stringify(pos['board']), pos['score'], tuple(map(lambda x: bool(x), pos['wc'])), tuple(map(lambda x: bool(x), pos['bc'])), pos['ep'], pos['kp'])
 
-    def value(self, move):
-        i, j = move
-        p, q = self.board[i], self.board[j]
-        # Actual move
-        score = pst[p][j] - pst[p][i]
-        # Capture
-        if q.islower():
-            score += pst[q.upper()][j]
-        # Castling check detection
-        if abs(j-self.kp) < 2:
-            score += pst['K'][j]
-        # Castling
-        if p == 'K' and abs(i-j) == 2:
-            score += pst['R'][(i+j)//2]
-            score -= pst['R'][A1 if j < i else H1]
-        # Special pawn stuff
-        if p == 'P':
-            if A8 <= j <= H8:
-                score += pst['Q'][j] - pst['P'][j]
-            if j == self.ep:
-                score += pst['P'][j+S]
-        return score
-
     def numpyify(self):
         # convert board to numpy array
         return np.array(map(lambda c: char_map[c], self.board)).astype(np.int32)
@@ -229,7 +120,6 @@ class Position(namedtuple('Position', 'board score wc bc ep kp')):
 if sys.version_info[0] == 2:
     input = raw_input
 
-
 def parse(c):
     fil, rank = ord(c[0]) - ord('a'), int(c[1]) - 1
     return A1 + fil - 10*rank
@@ -239,7 +129,6 @@ def render(i):
     rank, fil = divmod(i - A1, 10)
     return chr(fil + ord('a')) + str(-rank + 1)
 
-
 def print_pos(pos):
     print()
     uni_pieces = {'R':'♜', 'N':'♞', 'B':'♝', 'Q':'♛', 'K':'♚', 'P':'♟',
@@ -247,7 +136,6 @@ def print_pos(pos):
     for i, row in enumerate(pos.board.strip().split('\n ')):
         print(' ', 8-i, ' '.join(uni_pieces.get(p, p) for p in row))
     print('    a b c d e f g h \n\n')
-
 
 def main():
     pos = Position(initial, 0, (True, True), (True, True), 0, 0)
@@ -295,7 +183,6 @@ def main():
         print("My move:", render(119-move[0]) + render(119-move[1]))
         pos = pos.move(move)
         pos = pos.rotate()
-
 
 if __name__ == '__main__':
     main()
